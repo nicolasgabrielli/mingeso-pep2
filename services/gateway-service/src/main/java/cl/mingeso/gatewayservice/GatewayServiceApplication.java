c63d29116aa94cf02e2e7decf2e81efaff1aee54
nicolasgabrielli <nicolas.gabrielli@usach.cl>
2023-06-13 02:14:00 -0400

    Se quito el discoveryclient

--- services/gateway-service/src/main/java/cl/mingeso/gatewayservice/GatewayServiceApplication.java
+++ services/gateway-service/src/main/java/cl/mingeso/gatewayservice/GatewayServiceApplication.java
@@ -2,10 +2,9 @@ package cl.mingeso.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
--- services/summary-service/src/main/java/cl/mingeso/summaryservice/SummaryServiceApplication.java
+++ services/summary-service/src/main/java/cl/mingeso/summaryservice/SummaryServiceApplication.java
@@ -2,10 +2,8 @@ package cl.mingeso.summaryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SummaryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SummaryServiceApplication.class, args);
